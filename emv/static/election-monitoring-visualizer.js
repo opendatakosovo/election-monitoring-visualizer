f8bcaa6aead15d430b9eb5321ff23c6ba26e9394
legeorges <georges.labreche@gmail.com>
2014-08-25 12:37:55 -0700

    Fixed chart layouts and replaced tables with unordered lists

--- emv/static/election-monitoring-visualizer.js
+++ emv/static/election-monitoring-visualizer.js
@@ -1,79 +1,44 @@
-google.load("visualization", "1", {packages:["corechart", "table"]});	
+google.load("visualization", "1", {packages:["corechart", "columnchart", "table"]});	
 
+// Visualize all data that we want to visualize
+function visualizeData(communeSlug, pollingStationSlug){
+	
+	clearPreviouslyGeneratedDataVisualization();
+
+	var url = '/observations' + window.location.pathname + "/" + communeSlug + '/' + pollingStationSlug
+
+	$.get(url, function(data) {
+
+		$.each(data, function(index, observation) {
+
+			$("#dataVisualizationContainer").append('<div id="section-' + index + '"></div>');
+	
+			var roomNumer =  observation.pollingStation.roomNumber;
+			drawSectionHeader(index, roomNumer);
+
+			var voters = observation.votingProcess.voters.howManyVotedBy;
+			drawHowManyVotedByBarChart(index, voters);
+
+			var kvvMembers = observation.preparation.votingMaterialsPlacedInAndOutVotingStation.kvvMembers;
+			drawKvvMembersPieChart(index, kvvMembers);			
+			
+			var irregularities = observation.irregularities;
+			drawIrregularitiesTable(index, irregularities);
+
+			var votingProcess = observation.votingProcess.voters;
+			drawVotingProcessTable(index, votingProcess);
+
+			var missingMaterial = observation.preparation.missingMaterial;
+			drawMissingMaterialTable(index, missingMaterial);
+
+		});	
+	});
+}
 
 function drawSectionHeader(index, roomNumber){
-	var containerDivId = 'sectionHeader-' + index;
-	$("#dataVisualizationContainer").append("<h2 id='" + containerDivId + "'>Room " + roomNumber + ":</h2>");
-}
-
-// Draw the KVV members gender distribution pit chart
-function drawKvvMembersPieChart(index, kvvMembers){
-	var kvvMembersTotal = kvvMembers.total;
-	var kvvMembersFemale = kvvMembers.female;
-
-	// create div container for the pie chart
-	// e.g. <div id="kvvMembersPieChart" style="width: 900px; height: 500px;"></div>
-	var chartContainerDivId = 'kvvMembersPieChart-' + index;
-	$("#dataVisualizationContainer").append("<div id='" + chartContainerDivId + "'  class='kvvMembersPieChart'></div>");
-
-	var kvvMembersMale = kvvMembersTotal - kvvMembersFemale;
-
-	var data = google.visualization.arrayToDataTable([
-		['Gender', 'Number'],
-		['Male', kvvMembersMale],
-		['Female', kvvMembersFemale]
-	]);
-
-	var options = {
-	  	title: 'Gender Distribution of KVV Members',
-		is3D: true,
-	};
-
-	var chart = new google.visualization.PieChart(document.getElementById(chartContainerDivId));
-	chart.draw(data, options);
-}
-
-// Draw election process irregularities table
-function drawIrregularitiesTable(index, irregularities){
-
-	var chartContainerDivId = 'irregularities-' + index;
-	$("#dataVisualizationContainer").append("<div class='tables'>").append("<div id='" + chartContainerDivId + "' class='irregularities'></div>");
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Irregularity');
-    data.addColumn('boolean', 'Occured');
-    data.addRows([
-		['Unauthorized person stayed at the polling station', translateIrregularity(irregularities.unauthorizedPersonsStayedAtTheVotingStation)],
-		['Allowed to vote',  translateIrregularity(irregularities.allowedToVote)],
-		['Photographed ballot', translateIrregularity(irregularities.photographedBallot)],
-		['Accidents during the voting process',  translateIrregularity(irregularities.anyAccidentHappenedDuringTheProcess)],
-		['Violence in the polling station', translateIrregularity(irregularities.violenceInTheVotingStation)],
-		['More than one person the the voting cabin',  translateIrregularity(irregularities.moreThanOnePersonBehindTheCabin)],
-		['More than one ballot inserted', translateIrregularity(irregularities.insertedMoreThanOneBallot)],
-		['Cases of closed polling station',  translateIrregularity(irregularities.hasTheVotingStationBeenClosedInAnyCase)],
-		['Political propaganda inside the voting station',  translateIrregularity(irregularities.politicalPropagandaInsideTheVotingStation)],
-		['Attempted to vote more than once',  translateIrregularity(irregularities.attemptToVoteMoreThanOnce)]
-    ]);
-
-	var options = {
-	};
-
-
-	//FIXME: what about caseVotingOutsideTheCabin?
-
-    var table = new google.visualization.Table(document.getElementById(chartContainerDivId));
-    table.draw(data, options);
-}
-
-function translateIrregularity(irregularity){
-	if(irregularity == "N/A"){
-		return false;
-	}
-	else if(irregularity == true){
-		return true;
-	}else{
-		return false;
-	}
+	var sectionDivId = 'section-' + index;
+	var sectionHeaderDivId = 'header-' + index;
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + sectionDivId + "' style='display:inline;'><h3 id='" + sectionHeaderDivId + "'>Room " + roomNumber + ":</h3></div>");
 
 }
 
@@ -85,7 +50,7 @@ function drawHowManyVotedByBarChart(index,voters){
 
 	// create div container for the bar chart
 	var chartContainerDivId = 'votersByHourBarChart-' + index;
-	$("#dataVisualizationContainer").append("<div id='" + chartContainerDivId + "'  class='votersByHourBarChart'></div>");
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + chartContainerDivId + "'></div>");
 
 	var data = google.visualization.arrayToDataTable([
 		['Hour', 'Value',{ role: "style" }],
@@ -103,10 +68,12 @@ function drawHowManyVotedByBarChart(index,voters){
                        2]);
 
 	var options = {
-	  	title: 'Ballots Casted For Given Hours',
-		is3D: true,
-		bar: {groupWidth: "95%"},
-        legend: { position: "none" },
+	  	'title': 'Ballots Casted For Given Hours',
+		'is3D': true,
+		'bar': {groupWidth: "95%"},
+        'legend': { position: "none" },
+  		'height':700,
+		'width':600
 	};
 	
 
@@ -114,43 +81,107 @@ function drawHowManyVotedByBarChart(index,voters){
 	chart.draw(data, options);
 }
 
+// Draw the KVV members gender distribution pit chart
+function drawKvvMembersPieChart(index, kvvMembers){
+	var kvvMembersTotal = kvvMembers.total;
+	var kvvMembersFemale = kvvMembers.female;
+
+	// create div container for the pie chart
+	// e.g. <div id="kvvMembersPieChart" style="width: 900px; height: 500px;"></div>
+	var chartContainerDivId = 'kvvMembersPieChart-' + index;
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + chartContainerDivId + "' style='width:50%'></div>");
+
+	var kvvMembersMale = kvvMembersTotal - kvvMembersFemale;
+
+	var data = google.visualization.arrayToDataTable([
+		['Gender', 'Number'],
+		['Male', kvvMembersMale],
+		['Female', kvvMembersFemale]
+	]);
+
+	var options = {
+	  	'title': 'Gender Distribution of KVV Members',
+		'is3D': true,
+  		'height':400,
+		'width':800
+	};
+
+	var chart = new google.visualization.PieChart(document.getElementById(chartContainerDivId));
+	chart.draw(data, options);
+}
+
+// Draw election process irregularities table
+function drawIrregularitiesTable(index, irregularities){
+
+	// Create the containers.
+	var containerDivId = 'irregularities-' + index;
+	var listId = 'irregularity-list-' + index;
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + containerDivId + "'></div>");
+
+	// Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Irregularity');
+    data.addColumn('boolean', 'Occured');
+    data.addRows([
+		['Unauthorized person stayed at the polling station.', translateIrregularity(irregularities.unauthorizedPersonsStayedAtTheVotingStation)],
+		['Allowed to vote.',  translateIrregularity(irregularities.allowedToVote)],
+		['Photographed ballot.', translateIrregularity(irregularities.photographedBallot)],
+		['Accidents during the voting process.',  translateIrregularity(irregularities.anyAccidentHappenedDuringTheProcess)],
+		['Violence in the polling station.', translateIrregularity(irregularities.violenceInTheVotingStation)],
+		['More than one person in the voting cabin.',  translateIrregularity(irregularities.moreThanOnePersonBehindTheCabin)],
+		['More than one ballot inserted.', translateIrregularity(irregularities.insertedMoreThanOneBallot)],
+		['Cases of closed polling station.',  translateIrregularity(irregularities.hasTheVotingStationBeenClosedInAnyCase)],
+		['Political propaganda inside the voting station.',  translateIrregularity(irregularities.politicalPropagandaInsideTheVotingStation)],
+		['Attempted to vote more than once.',  translateIrregularity(irregularities.attemptToVoteMoreThanOnce)]
+    ]);
+
+	renderListOfObservedYesNoIssues(data, containerDivId, listId, 'Observed Irregularities:');
+
+}
+
+//#FIXME: data should be sanitized so that we don't have to do this.
+function translateIrregularity(irregularity){
+	if(irregularity == "N/A"){
+		return false;
+	}
+	else if(irregularity == true){
+		return true;
+	}else{
+		return false;
+	}
+}
+
 function drawVotingProcessTable(index, votingProcess){
 	
-
-	var chartContainerDivId = 'votingProcess-' + index;
-	$("#dataVisualizationContainer").append("<div id='" + chartContainerDivId + "' class='votingProcess'></div>");
+	var containerDivId = 'voting-process-' + index;
+	var listId = 'voting-process-list-' + index;
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + containerDivId + "'></div>");
 
 	var ultraControl= votingProcess.ultraVioletControl;
 	var fingerSprayed =votingProcess.fingerSprayed;
 	var identifiedWithDocument = votingProcess.identifiedWithDocument;
 
-
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'VotingProcess');
     data.addColumn('string', 'Occured');
     data.addRows([
-		['Ultra Violet Control', ultraControl ],
-		['Finger Sprayed',  fingerSprayed],
-		['Identifed with Document ID', identifiedWithDocument]
+		['Ultra Violet Control.', ultraControl ],
+		['Finger Sprayed.',  fingerSprayed],
+		['Identifed with Document ID.', identifiedWithDocument]
 		
     ]);
 
-	var options = {
-	};
+	renderListOfObservedFrequencyIssues(data, containerDivId, listId, 'Control Flaws:');
 
-
-	//FIXME: what about sealedBallot?
-
-    var table = new google.visualization.Table(document.getElementById(chartContainerDivId));
-    table.draw(data, options);
 }
+
 
 function drawMissingMaterialTable(index, missingMaterial){
 		
+	var containerDivId = 'missing-material-' + index;
+	var listId = 'missing-material-list-' + index;
+	$("#dataVisualizationContainer #section-" + index).append("<div id='" + containerDivId + "'></div>");	
 
-	var chartContainerDivId = 'missingMaterial-' + index;
-	$("#dataVisualizationContainer").append("<div id='" + chartContainerDivId + "' class='missingMaterial'></div>");
-	
 	var ballotBox = missingMaterial.ballotBox;
 	var votersBook = missingMaterial.votersBook;
 	var spray = missingMaterial.spray;
@@ -162,29 +193,74 @@ function drawMissingMaterialTable(index, missingMaterial){
 	var ballots = missingMaterial.ballots;
 
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Missing Material');
+    data.addColumn('string', 'MissingMaterial');
     data.addColumn('boolean', 'Occured');
     data.addRows([
-		['Ballot Box', ballotBox ],
-		['Voters Book',  votersBook],
-		['Spray', spray],
-		['Voters List', votersList],
-		['Envelopes for Condition Voters', envelopsForConditionVoters],
-		['UV Lamp', uvLamp],
-		['Voting Cabin', votingCabin],
-		['Stamp', stamp],
-		['Ballots', ballots]
+		['Ballot Box.', ballotBox ],
+		['Voters Book.',  votersBook],
+		['Spray.', spray],
+		['Voters List.', votersList],
+		['Envelopes for Condition Voters.', envelopsForConditionVoters],
+		['UV Lamp.', uvLamp],
+		['Voting Cabin.', votingCabin],
+		['Stamp.', stamp],
+		['Ballots.', ballots]
 		
     ]);
 
-	var options = {
-	  	width: '450px'
-	};
+	renderListOfObservedYesNoIssues(data, containerDivId, listId, 'Missing Materials:');
+}
 
+function renderListOfObservedYesNoIssues(data, containerDivId, listId, title){
+	// Loop the data table and list the irregularities that are true.
+	var atLeastOneItem = false;
 
+	for(var i = 0; i < data.getNumberOfRows(); i++){
+		var val = data.getValue(i, 1);
+		if(val == true){
+		
+			// Render list header and list container.
+			if(atLeastOneItem == false){
+				$('#' + containerDivId).append('<strong>' + title + '</strong>');
+				$('#' + containerDivId).append("<ul id='" + listId +"'></ul>");
+				
+				atLeastOneItem = true;			
+			}
 
-    var table = new google.visualization.Table(document.getElementById(chartContainerDivId));
-    table.draw(data, options);
+			// Add issue to the list.
+			var issue = data.getValue(i, 0);
+			$("#" + listId).append('<li>' + issue + '</li>');
+		}
+	}
+}
+
+function renderListOfObservedFrequencyIssues(data, containerDivId, listId, title){
+	// Loop the data table and list the irregularities that are true.
+	var atLeastOneItem = false;
+
+	for(var i = 0; i < data.getNumberOfRows(); i++){
+		var val = data.getValue(i, 1);
+		if(val != 'always'){
+		
+			// Render list header and list container.
+			if(atLeastOneItem == false){
+				$('#' + containerDivId).append('<strong>' + title + '</strong>');
+				$('#' + containerDivId).append("<ul id='" + listId +"'></ul>");
+				
+				atLeastOneItem = true;			
+			}
+
+			// Add item to the list.
+			var issue = data.getValue(i, 0);
+			capitalizedVal = capitaliseFirstLetter(val);
+			$("#" + listId).append('<li>' + capitalizedVal + ' ' + issue + '</li>');
+		}
+	}
+}
+
+function capitaliseFirstLetter(string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
  
 // Initialize polling station name dropdown list
@@ -209,39 +285,6 @@ function initPollingStationDropdown(communeSlug){
 	visualizeData(communeSlug, pollingStationSlug);
 }
 
-// Visualize all data that we want to visualize
-function visualizeData(communeSlug, pollingStationSlug){
-	
-	clearPreviouslyGeneratedDataVisualization();
-
-	var url = '/observations' + window.location.pathname + "/" + communeSlug + '/' + pollingStationSlug
-
-	$.get(url, function(data) {
-
-		$.each(data, function(index, observation) {
-	
-			var kvvMembers = observation.preparation.votingMaterialsPlacedInAndOutVotingStation.kvvMembers;
-			drawKvvMembersPieChart(index, kvvMembers);
-
-			var roomNumer =  observation.pollingStation.roomNumber;
-			drawSectionHeader(index, roomNumer);
-
-			var voters = observation.votingProcess.voters.howManyVotedBy;
-			drawHowManyVotedByBarChart(index, voters);
-			
-			var irregularities = observation.irregularities;
-			drawIrregularitiesTable(index, irregularities);
-
-			var votingProcess= observation.votingProcess.voters;
-			drawVotingProcessTable(index, votingProcess);
-
-			var missingMaterial= observation.preparation.missingMaterial;
-			drawMissingMaterialTable(index, missingMaterial); 
-			
-		});
-		
-	});
-}
 
 function clearPreviouslyGeneratedDataVisualization(){
 	$("#appInfoContainer").html('');
